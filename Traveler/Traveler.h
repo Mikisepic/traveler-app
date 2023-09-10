18c01d928d22f40b5ccb6630da5e01899a334c48
Mikas Pupeikis <mikas.pup@gmail.com>
2023-09-10 20:32:26 +0300

    Initial Commit

---
+++ Traveler/Traveler.h
@@ -0,0 +1,18 @@
+//
+//  Traveler.h
+//  Traveler
+//
+//  Created by Mikas Pupeikis on 2023-09-10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Traveler.
+FOUNDATION_EXPORT double TravelerVersionNumber;
+
+//! Project version string for Traveler.
+FOUNDATION_EXPORT const unsigned char TravelerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Traveler/PublicHeader.h>
+
+

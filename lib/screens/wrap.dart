89f0bca99b52ef10c3e671858abfae052f5d7e8c
Mikas Pupeikis <mikas.pup@gmail.com>
2023-10-01 18:01:51 +0300

    wrap scaffold

---
+++ lib/screens/wrap.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class WrapScaffold extends StatelessWidget {
+  const WrapScaffold({
+    super.key,
+    required this.label,
+    required this.body,
+    this.floatingActionButton,
+    this.detailsPath,
+  });
+
+  final String label;
+  final Widget body;
+  final Widget? floatingActionButton;
+  final String? detailsPath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        title: Text('Tab root - $label'),
+      ),
+      body: body,
+      floatingActionButton: floatingActionButton,
+    );
+  }
+}

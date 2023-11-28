21f5452fffdc6c6ca197ed88ed2c88a02a82ce18
Mikas Pupeikis <mikas.pup@gmail.com>
2023-11-28 18:36:54 +0200

    trip model upgrade

--- lib/models/trip.model.dart
+++ lib/models/trip.model.dart
@@ -3,14 +3,15 @@ import 'marker.model.dart';
 class Trip {
   final String id;
   final String title;
-  final bool isPrivate;
   final String description;
   final List<MapboxMarker> markers;
+  final bool isPrivate;
 
-  Trip(
-      {required this.id,
-      required this.title,
-      this.isPrivate = false,
-      required this.description,
-      required this.markers});
+  Trip({
+    required this.id,
+    required this.title,
+    required this.description,
+    this.markers = const [],
+    this.isPrivate = false,
+  });
 }
